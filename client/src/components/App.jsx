d178d85ffaffebecfcd6c7557ad2002fa931847a
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-06-11 10:17:17 -0700

    Initial commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }  
+
+  render () {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App
