c46d435a9c711babc5cd28878098f256a39e4bdb
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-06-11 00:09:55 -0700

    Setup Webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
