0209193b517879af06222d48a6e7fcec93740b9a
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-06-11 23:16:30 -0700

    Create 2 get request to handle gene names and gene information

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import axios from 'axios';
+import AutoSuggest from './AutoSuggest.jsx';
+import InfoTable from './InfoTable.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-  }  
+    this.state = {
+      results: [],
+      geneNames: [],
+    };
+    this.getGeneInformation = this.getGeneInformation.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getGeneNames = this.getGeneNames.bind(this);
+  }
+  
+  componentDidMount() {
+    this.getGeneNames();
+  }
+
+  getGeneInformation(value) {
+    axios.get(`/gene/${value}`)
+    .then((results) => this.setState({results: results.data.rows}))
+    .catch((err) => console.log(err));
+  }
+
+  getGeneNames(){
+    axios.get('/geneName')
+    .then((results) => this.setState({geneNames: results.data}))
+    .then(() => this.test)
+    .catch((err) => console.log(err));
+  }
+
+  handleSubmit(e, text) {
+    this.getGeneInformation(text);
+    e.preventDefault();
+  }
 
   render () {
     return (
-      <div>
-        Hello
+      <div className='parentComponent'>
+        <AutoSuggest className='autoSuggest' handleSubmit={this.handleSubmit} genes={this.state.geneNames}/>
+        <InfoTable geneInformation={this.state.results}/>
       </div>
     );
   }
